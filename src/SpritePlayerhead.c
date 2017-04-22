510b59f961d158809ba073eafb8d039ab7297e39
sergeeo <sergeeo@gmail.com>
2017-04-22 13:12:09 +0200

    reference for the head is not scroll_target, but an extern definition of the main sprite struct

--- src/SpritePlayerhead.c
+++ src/SpritePlayerhead.c
@@ -9,13 +9,15 @@
 
 UINT8 bank_SPRITE_PLAYERHEAD = 2;
 
+extern struct Sprite* sprite_player;
+
 void Start_SPRITE_PLAYERHEAD() {
 
 }
 
 void Update_SPRITE_PLAYERHEAD() {
-	THIS->x = scroll_target->x;
-	THIS->y = scroll_target->y - 15u;
+	THIS->x = sprite_player->x;
+	THIS->y = sprite_player->y - 15u;
 }
 
 void Destroy_SPRITE_PLAYERHEAD() {
