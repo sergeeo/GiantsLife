171fc05f84d95444993ff4f2b7282974f9414e62
sergeeo <sergeeo@gmail.com>
2017-04-23 16:15:18 +0200

    enemies move :)

--- src/SpritePlane.c
+++ src/SpritePlane.c
@@ -8,6 +8,8 @@ UINT8 bank_SPRITE_PLANE = 2;
 const UINT8 anim_planedestroy[] = {5, 1, 2, 1, 2, 3};
 
 extern struct Sprite* sprite_player;
+UINT8 plane_speed = 2u;
+UINT8 plane_direction = 0u;
 
 typedef enum  {
 	PLANE_NORMAL,
@@ -22,6 +24,14 @@ void Start_SPRITE_PLANE() {
 	THIS->coll_w = 14u;
 	THIS->coll_h = 10u;
 	plane_state = PLANE_NORMAL;
+	
+	if(sprite_player->x > THIS->x){
+		THIS->flags = OAM_VERTICAL_FLAG;
+		plane_direction = 0u;
+	} else if (sprite_player->x < THIS->x){
+		THIS->flags = 0u;
+		plane_direction = 1u;
+	}
 }
 
 void Update_SPRITE_PLANE() {
@@ -45,6 +55,14 @@ void Update_SPRITE_PLANE() {
 						SetSpriteAnim(THIS, anim_planedestroy, 6u);
 					}
 				}
+			}
+			
+			if(plane_direction == 0u){
+				THIS->flags = OAM_VERTICAL_FLAG;
+				THIS->x = THIS->x + plane_speed;
+			} else {
+				THIS->flags = 0u;
+				THIS->x = THIS->x - plane_speed;
 			}
 			break;
 	}
--- src/SpriteTank.c
+++ src/SpriteTank.c
@@ -9,6 +9,8 @@ const UINT8 anim_walktank[] = {3, 0, 1, 2};
 const UINT8 anim_destroytank[] = {5, 3, 4, 3, 4, 5};
 
 extern struct Sprite* sprite_player;
+UINT8 tank_speed = 1u;
+UINT8 tank_direction = 0u;
 
 typedef enum  {
 	TANK_NORMAL,
@@ -23,6 +25,14 @@ void Start_SPRITE_TANK() {
 	THIS->coll_w = 14u;
 	THIS->coll_h = 10u;
 	tank_state = TANK_NORMAL;
+	
+	if(sprite_player->x > THIS->x){
+		THIS->flags = OAM_VERTICAL_FLAG;
+		tank_direction = 0u;
+	} else if (sprite_player->x < THIS->x){
+		THIS->flags = 0u;
+		tank_direction = 1u;
+	}
 }
 
 void Update_SPRITE_TANK() {
@@ -32,12 +42,13 @@ void Update_SPRITE_TANK() {
 	
 	switch(tank_state){
 		case TANK_DESTROY:
-			if(THIS->current_frame == 5) {
+			if(THIS->current_frame == 4) {
 				SpriteManagerRemove(THIS_IDX);
 				// SpriteManagerRemoveSprite(THIS);
 			}
 			break;
 		case TANK_NORMAL:
+			SetSpriteAnim(THIS, anim_walktank, 9u);
 			SPRITEMANAGER_ITERATE(i, spr) {
 				if(spr->type == SPRITE_PLAYER || spr->type == SPRITE_PLAYERBODY) {
 					if(CheckCollision(THIS, spr)) {
@@ -46,6 +57,13 @@ void Update_SPRITE_TANK() {
 						SetSpriteAnim(THIS, anim_destroytank, 3u);
 					}
 				}
+			}
+			if(tank_direction == 0u){
+				THIS->flags = OAM_VERTICAL_FLAG;
+				THIS->x = THIS->x + tank_speed;
+			} else {
+				THIS->flags = 0u;
+				THIS->x = THIS->x - tank_speed;
 			}
 			break;
 	}
